3da114172971be5b9c13a3ba62d49ec9b4acf445
samuelSoler99 <samuelsolerpico99@gmail.com>
2024-02-03 15:46:21 +0100

    añadiendo togle del menu lateral con zustand

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,9 +1,12 @@
+'use client'
 import { titleFont } from '@/config/fonts'
+import { useUiStore } from '@/store'
 import Link from 'next/link'
 import React from 'react'
 import { IoCartOutline, IoSearchOutline } from 'react-icons/io5'
 
 export const TopMenu = () => {
+    const openMenu = useUiStore(state => state.openSideMenu);
     return (
         <nav className='flex px-5 justify-between items-center w-full'>
             {/* Logo */}
@@ -38,7 +41,7 @@ export const TopMenu = () => {
                     </div>
                 </Link>
 
-                <button className='m-2 p-2 rounded-md transition-all hover:bg-gray-100'>
+                <button onClick={()=>{openMenu()}} className='m-2 p-2 rounded-md transition-all hover:bg-gray-100'>
                     Menu
                 </button>
             </div>
